bf38c00dc6d79ddd27961f1773eccf70aefaec5b
Haven Selph <havenselph@gmail.com>
2024-06-26 14:57:39 -0500

    minor performance tweaks

--- src/main.rs
+++ src/main.rs
@@ -31,13 +31,13 @@ struct Args {
     filename: Option<String>,
     #[arg(short, long)]
     debug: bool,
-    #[arg(short, long, value_enum, default_value_t=ErrorLevel::Normal)]
+    #[arg(long, value_enum, default_value_t=ErrorLevel::Normal)]
     error_level: ErrorLevel
 }
 
-fn interpret(debug: bool, filename: Arc<str>, contents: &str, reports: Rc<RefCell<Vec<ErrorReport>>>) -> i32 {
+fn interpret(debug: bool, filename: Arc<str>, contents: &str, reports: &mut Vec<ErrorReport>) -> i32 {
     let tokens = {
-        let mut lexer = Lexer::new(filename.clone(), contents, reports.clone());
+        let mut lexer = Lexer::new(filename.clone(), contents, reports);
         lexer.lex_tokens();
         if debug {
             for (i, token) in lexer.tokens.iter().enumerate() {
@@ -49,7 +49,7 @@ fn interpret(debug: bool, filename: Arc<str>, contents: &str, reports: Rc<RefCel
     };
 
     let ast = {
-        let mut parser = Parser::new(&tokens, reports.clone());
+        let mut parser = Parser::new(&tokens, reports);
         let Some(ast) = parser.parse() else { return 69; };
         if debug { println!("{}", ast) }
         if parser.had_error { return 69; }
@@ -60,11 +60,11 @@ fn interpret(debug: bool, filename: Arc<str>, contents: &str, reports: Rc<RefCel
     unimplemented!("Reached interpretation step, not yet finished.");
 }
 
-fn print_reports(level: ErrorLevel, filename: Arc<str>, contents: &String, reports: Rc<RefCell<Vec<ErrorReport>>>) {
+fn print_reports(level: ErrorLevel, filename: Arc<str>, contents: &String, reports: Vec<ErrorReport>) {
     let silent = level == ErrorLevel::Silent;
     let cache = (filename, Source::from(contents));
     let mut emitted_errors: usize = 0;
-    for report in reports.borrow().iter() {
+    for report in reports.iter() {
         if !silent {
             report.to_ariadne_report(level).eprint(cache.clone()).unwrap();
         }
@@ -87,14 +87,16 @@ fn main() {
     let args = Args::parse();
 
     if args.filename.is_some() {
-        let mut reports = Rc::new(RefCell::new(Vec::<ErrorReport>::new()));
+        let mut reports = Vec::<ErrorReport>::new();
         let arc_filename: Arc<str> = Arc::from(args.filename.unwrap());
 
         let mut contents = String::new();
         File::open(arc_filename.deref()).unwrap().read_to_string(&mut contents).unwrap();
 
-        let code = interpret(args.debug, arc_filename.clone(), &contents, reports.clone());
-        if !reports.borrow().is_empty() {
+        let code = {
+            interpret(args.debug, arc_filename.clone(), &contents, &mut reports)
+        };
+        if !reports.is_empty() {
             print_reports(args.error_level, arc_filename, &contents, reports);
         }
         exit(code);
