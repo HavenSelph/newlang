8f859129b47b3159352b871ab7ead62fb08a856b
Haven Selph <havenselph@gmail.com>
2024-06-26 14:56:01 -0500

    made error struct more generic

--- src/error.rs
+++ src/error.rs
@@ -10,23 +10,29 @@ pub type ResultErrorless<T> = std::result::Result<T, ()>;
 
 #[derive(PartialOrd, PartialEq, Copy, Clone, Debug, ValueEnum)]
 pub enum ErrorLevel {
-    /// Show no error messages at all.
+    /// Show messages at all.
     Silent,
-    /// Show only minimal context.
+    /// Just print the error.
     Compact,
-    /// Show all context.
+    /// Show most information.
     Normal,
+    // Show extra information.
+    Debug
 }
 
+#[derive(Clone)]
 pub struct ErrorReport {
     span: Span,
     title: String,
-    labels: Vec<ariadne::Label<Span>>
+    labels: Vec<ariadne::Label<Span>>,
+    debug_labels: Vec<ariadne::Label<Span>>,
+    note: Option<String>,
 }
 
 impl ErrorReport {
     pub fn new(kind: ErrorReportKind, span: Span, message: String) -> Self {
         let title = if kind != ErrorReportKind::Custom {
+            // span does not store a line or column, printing indexes will confuse most IDEs
             format!("{:?}: {}", kind, message)
         } else {
             message
@@ -34,7 +40,9 @@ impl ErrorReport {
         ErrorReport {
             span,
             title,
-            labels: Vec::new()
+            labels: Vec::new(),
+            debug_labels: Vec::new(),
+            note: None,
         }
     }
 
@@ -43,21 +51,39 @@ impl ErrorReport {
         self
     }
 
+    pub fn with_debug_label(mut self, label: ariadne::Label<Span>) -> Self {
+        self.debug_labels.push(label);
+        self
+    }
+
+    pub fn with_note(mut self, note: String) -> Self {
+        self.note = Some(note);
+        self
+    }
+
     pub fn to_ariadne_report(&self, level: ErrorLevel) -> Report<Span> {
-        match level {
+        let mut report = match level {
             ErrorLevel::Silent => unreachable!("Cannot make a silent ariadne report."),
             ErrorLevel::Compact => {
-                let report_kind = ReportKind::Custom(Box::leak(format!("{} Error", self.span.clone()).into_boxed_str()), Color::Red);
+                let report_kind = ReportKind::Custom(Box::leak(format!("[{}] Error", self.span.clone()).into_boxed_str()), Color::Red);
                 Report::build(report_kind, self.span.filename.clone(), self.span.start)
                 .with_message(self.title.clone())
                 .with_config(Config::default().with_compact(true))
-                .finish()
             },
             ErrorLevel::Normal => Report::build(ReportKind::Error, self.span.filename.clone(), self.span.start)
                 .with_message(self.title.clone())
+                .with_labels(self.labels.clone()),
+            ErrorLevel::Debug => Report::build(ReportKind::Error, self.span.filename.clone(), self.span.start)
+                .with_message(self.title.clone())
                 .with_labels(self.labels.clone())
-                .finish(),
-        }
+                .with_labels(self.debug_labels.clone())
+        };
+        report = if let Some(note) = self.note.clone() {
+            report.with_note(note)
+        } else {
+            report
+        };
+        report.finish()
     }
 }
 
